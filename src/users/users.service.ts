d28c4fa0285921d57ff488c81c856208a6fbfd59
usama-taj-dev <usamataj1998@gmail.com>
2025-12-16 23:56:33 +0500

    feat: implement user authentication with DTOs and validation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    console.log('Created user', user);
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    const user = this.repo.findOneBy({ id });
+    return user;
+  }
+
+  find(email: string) {
+    const user = this.repo.find({ where: { email } });
+    return user;
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not faound');
+    }
+    return this.repo.remove(user);
+  }
+}
