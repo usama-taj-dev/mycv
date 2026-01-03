900c552c1bdc64f9f4f4fd6bdf131fa054cb1558
usama-taj-dev <usamataj1998@gmail.com>
2026-01-03 23:44:34 +0500

    fix: update jest configuration and setup test file paths

--- test/setupTest.ts
+++ test/setupTest.ts
@@ -3,7 +3,7 @@ import { join } from 'path';
 
 global.beforeEach(async () => {
   try {
-    await rm(join(__dirname, '..', 'test.sqlite'));
+    await rm(join(__dirname, '..', 'test-db.sqlite'));
   } catch (err) {
     console.log(err);
   }
